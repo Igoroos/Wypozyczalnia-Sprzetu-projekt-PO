75010cf82a9a0aac08e9ab355060dd8f14b4c25a
igoros16 <igor10071@wp.pl>
2026-05-20 23:30:46 +0200

    Dodano klasę Sprzęt

---
+++ Wypozyczalnia_sprzetow/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace WypozyczalniaSprzetu
+{
+    public class Sprzet
+    {
+        public string Nazwa { get; set; }
+        public string Typ { get; set; }
+        public string Rozmiar { get; set; }
+        public string Marka { get; set; }
+        public string StanTechniczny { get; set; }
+        public bool Dostepny { get; set; }
+
+        public bool SprawdzDostepnosc()
+        {
+            return Dostepny;
+        }
+        public void OznaczJakoWypozyczony()
+        {
+            Dostepny = false;
+        }
+        public void OznaczJakoDostepny()
+        {
+            Dostepny = true;
+        }
+        public void ZmienStanTechniczny(string nowyStan)
+        {
+            StanTechniczny = nowyStan;
+        }
+    }
+}
