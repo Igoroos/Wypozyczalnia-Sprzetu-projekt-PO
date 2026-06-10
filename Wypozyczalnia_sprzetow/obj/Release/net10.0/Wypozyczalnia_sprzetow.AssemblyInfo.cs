f107fb076703ce19be4f87480a2d1cae758db268
Igor <igor10071@wp.pl>
2026-06-10 17:41:53 +0200

    dodanie uml

--- Wypozyczalnia_sprzetow/obj/Release/net10.0/Wypozyczalnia_sprzetow.AssemblyInfo.cs
+++ Wypozyczalnia_sprzetow/obj/Release/net10.0/Wypozyczalnia_sprzetow.AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Wypozyczalnia_sprzetow")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8cbbd3ca85f674527c83fdd396eae75bec66e2af")]
 [assembly: System.Reflection.AssemblyProductAttribute("Wypozyczalnia_sprzetow")]
